2a4cad576c1aacf6d52ceae5bb2b683207c2a4c3
mingli <mingli2015md@gmail.com>
2020-02-15 12:11:04 +0100

    implement login with email

--- src/app/user/google-signin.directive.ts
+++ src/app/user/google-signin.directive.ts
@@ -1,6 +1,6 @@
 import {Directive, HostListener} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
 @Directive({
