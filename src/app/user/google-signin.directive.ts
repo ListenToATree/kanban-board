a452ec776f18eefcacc96d68820f41e1722a8f62
mingli <mingli2015md@gmail.com>
2020-02-15 11:17:08 +0100

    implement login with google auth

---
+++ src/app/user/google-signin.directive.ts
@@ -0,0 +1,18 @@
+import {Directive, HostListener} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import * as firebase from 'firebase';
+import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
+
+@Directive({
+  selector: '[appGoogleSignin]'
+})
+export class GoogleSigninDirective {
+
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+  @HostListener('click')
+  onclick() {
+    this.afAuth.auth.signInWithPopup(new GoogleAuthProvider());
+  }
+}
