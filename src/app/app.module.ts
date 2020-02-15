d7ea4924523d4df1a9355997baadc4f898400cf4
mingli <mingli2015md@gmail.com>
2020-02-15 10:38:38 +0100

    init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
-import { HomePageComponent } from './home-page/home-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD23LXNgAlFgJMyVqwyPkPpXQ69ilIFxK8',
+    authDomain: 'kanban-board-angular9.firebaseapp.com',
+    databaseURL: 'https://kanban-board-angular9.firebaseio.com',
+    projectId: 'kanban-board-angular9',
+    storageBucket: 'kanban-board-angular9.appspot.com',
+    messagingSenderId: '207288654374',
+    appId: '1:207288654374:web:7cfc27852a23b66f8024e7',
+    measurementId: 'G-TBPYT5T0FQ'
+  }
 };
